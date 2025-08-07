1e6da4f058407c1c536bd98197bbc29abd2cc7d8
Alraf45 <143483629+Alraf45@users.noreply.github.com>
2025-08-07 13:32:10 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\HomeController as UserHomeController;
+use App\Http\Controllers\User\ProductController as UserProductController;
+use App\Http\Controllers\User\CartController as UserCartController;
+use App\Http\Controllers\User\CheckoutController as UserCheckoutController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\ProductController as AdminProductController;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\ColorController as AdminColorController;
+use Illuminate\Support\Facades\Auth;
 
+// Halaman Welcome Default (opsional)
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 // User routes
-Route::get('/', [User\HomeController::class, 'index'])->name('home');
-Route::get('/produk/{id}', [User\ProductController::class, 'show']);
-Route::get('/cart', [User\CartController::class, 'index']);
-Route::get('/checkout', [User\CheckoutController::class, 'index']);
+Route::get('/home', [UserHomeController::class, 'index'])->name('home');
+Route::get('/produk/{id}', [UserProductController::class, 'show'])->name('product.show');
+Route::get('/cart', [UserCartController::class, 'index'])->name('cart.index');
+Route::get('/checkout', [UserCheckoutController::class, 'index'])->name('checkout.index');
 
 // Admin routes
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
-    Route::get('/dashboard', [Admin\DashboardController::class, 'index']);
-    Route::resource('/products', Admin\ProductController::class);
-    Route::resource('/colors', Admin\ColorController::class);
-    Route::resource('/categories', Admin\CategoryController::class);
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+    Route::resource('/products', AdminProductController::class);
+    Route::resource('/categories', AdminCategoryController::class);
+    Route::resource('/colors', AdminColorController::class);
 });
 
+// Auth
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard');

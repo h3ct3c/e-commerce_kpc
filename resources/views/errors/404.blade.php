42214250cfc135207c23bbd020f80941706379a9
h3ct3c <hect3ch@gmail.com>
2025-08-07 09:52:58 +0700

    rapihin

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>404 Not Found</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-white flex items-center justify-center min-h-screen text-gray-700 p-6">
+  <div class="text-center max-w-md">
+    <img src="/img/Wibu stress.jpeg" alt="404" class="w-screen mx-auto mb-10">
+    <h1 class="text-6xl font-bold text-black mb-6">404</h1>
+    <a href="{{ url('/') }}" class="inline-block bg-yellow-200 text-yellow-900 px-6 py-3 rounded-lg hover:bg-yellow-500 transition mt-4">
+      Back to Home
+    </a>
+  </div>
+</body>
+</html>
